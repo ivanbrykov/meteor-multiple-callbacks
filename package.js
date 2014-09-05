b7ac97b53ab4ea19f2525d500b2d5506ee718d3d
Ivan Brykov <ivan.ru@gmail.com>
2014-09-06 02:27:37 +0400

    update package.js to comply with Meteor requirements

--- package.js
+++ package.js
@@ -1,17 +1,20 @@
 Package.describe({
-  summary: "Multiple template callbacks implementation for Meteor"
+  summary: "Multiple template callbacks implementation for Meteor",
+  version: "0.1.4",
+  name: "xorax:multiple-callbacks",
+  git: "https://github.com/brykov/meteor-multiple-callbacks.git"
 });
 
 Package.on_use(function (api) {
 //  api.export('multiple-callbacks');
 
-  api.use('underscore', 'client');
-  api.use('templating', 'client');
+  api.use('underscore@1.0.0', 'client');
+  api.use('templating@1.0.5', 'client');
   api.add_files(['multiple-callbacks.js'], 'client');
 });
 
 Package.on_test(function (api) {
-  api.use(['tinytest', 'test-helpers', 'templating', 'multiple-callbacks'], 'client');
+  api.use(['tinytest@1.0.1', 'test-helpers@1.0.1', 'templating@1.0.5', 'xorax:multiple-callbacks@0.1.4'], 'client');
   api.add_files('multiple-callbacks-test.html', 'client');
   api.add_files('multiple-callbacks-test.js', 'client');
 });
